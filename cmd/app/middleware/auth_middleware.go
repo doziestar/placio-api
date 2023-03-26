634294b02e893284909ffc180fdfb047c1abf4cc
Chidozie C. Okafor <chidosiky2015@gmail.com>
2023-03-26 12:28:22 +0100

    check token expiration

--- cmd/app/middleware/auth_middleware.go
+++ cmd/app/middleware/auth_middleware.go
@@ -24,21 +24,6 @@ type JWTClaims struct {
 	jwt.StandardClaims
 }
 
-//
-//func GenerateToken(data map[string]interface{}, secret string, duration int) (string, error) {
-//	claims := JWTClaims{
-//		AccountID:  uint(data["accountId"].(float64)),
-//		UserID:     uint(data["userId"].(float64)),
-//		Permission: data["permission"].(string),
-//		Provider:   data["provider"].(string),
-//		StandardClaims: jwt.StandardClaims{
-//			ExpiresAt: jwt.At(time.Now().Add(time.Minute * time.Duration(duration))),
-//		},
-//	}
-//	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-//	return token.SignedString([]byte(secret))
-//}
-
 func VerifyToken(tokenString string, secret string) (*JWTClaims, error) {
 	token, err := jwt.ParseWithClaims(tokenString, &JWTClaims{}, func(token *jwt.Token) (interface{}, error) {
 		return []byte(secret), nil
@@ -52,65 +37,6 @@ func VerifyToken(tokenString string, secret string) (*JWTClaims, error) {
 		return nil, errors.New("invalid token")
 	}
 }
-
-//func Verify(permission string, scope string) fiber.Handler {
-//	return func(c *fiber.Ctx) error {
-//		header := c.Get("Authorization")
-//
-//		if header == "" {
-//			if permission == "public" {
-//				return c.Next()
-//			} else {
-//				return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-//					"message": "No authorization header provided",
-//				})
-//			}
-//		}
-//
-//		parts := strings.Split(header, " ")
-//
-//		if len(parts) != 2 || parts[0] != "Bearer" {
-//			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-//				"message": "Unrecognised header type",
-//			})
-//		}
-//
-//		token := parts[1]
-//
-//		claims, err := jwt.ParseWithClaims(token, &JwtCustomClaims{}, func(token *jwt.Token) (interface{}, error) {
-//			return []byte(os.Getenv("TOKEN_SECRET")), nil
-//		})
-//
-//		if err != nil {
-//			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-//				"message": "Invalid token",
-//			})
-//		}
-//
-//		if claims.Valid && claims.Claims.(*JwtCustomClaims).AccountId != "" &&
-//			claims.Claims.(*JwtCustomClaims).UserId != "" &&
-//			claims.Claims.(*JwtCustomClaims).Permission != "" &&
-//			claims.Claims.(*JwtCustomClaims).Provider != "" {
-//
-//			if permission == "public" || permissions[claims.Claims.(*JwtCustomClaims).Permission][permission] {
-//				c.Locals("account", claims.Claims.(*JwtCustomClaims).AccountId)
-//				c.Locals("user", claims.Claims.(*JwtCustomClaims).UserId)
-//				c.Locals("permission", claims.Claims.(*JwtCustomClaims).Permission)
-//				c.Locals("provider", claims.Claims.(*JwtCustomClaims).Provider)
-//				return c.Next()
-//			} else {
-//				return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-//					"message": "User doesn't have permission",
-//				})
-//			}
-//
-//		} else {
-//			return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-//				"message": "Invalid auth token",
-//			})
-//		}
-//	}
-//}
 
 func Verify(permission string) fiber.Handler {
 	logger.Info(context.Background(), fmt.Sprintf("permission: %s", permission))
