4cda36f0230ce1988c010965b393e5d1a62d6647
Chidozie C. Okafor <39489124+doziestar@users.noreply.github.com>
2023-04-21 22:03:27 +0100

    add comments, like, media, post and account follow test

---
+++ cmd/app/models/comments.go
@@ -0,0 +1,12 @@
+package models
+
+import "time"
+
+type Comment struct {
+	ID        string    `gorm:"primaryKey,unique"`
+	AccountID string    `gorm:"index"`
+	PostID    string    `gorm:"index"`
+	Content   string    `gorm:"type:text"`
+	CreatedAt time.Time `gorm:"column:created_at"`
+	UpdatedAt time.Time `gorm:"column:updated_at"`
+}
---
+++ cmd/app/models/like.go
@@ -0,0 +1,11 @@
+package models
+
+import "time"
+
+type Like struct {
+	ID        string    `gorm:"primaryKey,unique"`
+	AccountID string    `gorm:"index"`
+	PostID    string    `gorm:"index"`
+	CreatedAt time.Time `gorm:"column:created_at"`
+	UpdatedAt time.Time `gorm:"column:updated_at"`
+}
