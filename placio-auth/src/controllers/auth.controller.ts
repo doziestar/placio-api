0929bc01b98ca798168b0503bd130fe53dfa92f0
Chidozie C. Okafor <39489124+doziestar@users.noreply.github.com>
2023-05-11 07:01:46 +0100

    working on auth system

--- placio-auth/src/controllers/auth.controller.ts
+++ placio-auth/src/controllers/auth.controller.ts
@@ -1,44 +1,178 @@
-import { NextFunction, Request, Response } from 'express';
-import { Container } from 'typedi';
-import { RequestWithUser } from '@interfaces/auth.interface';
-import { User } from '@interfaces/users.interface';
-import { AuthService } from '@services/auth.service';
+import { Request, Response, NextFunction } from 'express';
+
+const authService = new AuthService();
 
 export class AuthController {
-  public auth = Container.get(AuthService);
-
-  public signUp = async (req: Request, res: Response, next: NextFunction) => {
+  async signin(req: Request, res: Response, next: NextFunction) {
     try {
-      const userData: User = req.body;
-      const signUpUserData: User = await this.auth.signup(userData);
-
-      res.status(201).json({ data: signUpUserData, message: 'signup' });
+      const result = await authService.signin(req, res);
+      res.status(result.status).send(result.data);
     } catch (error) {
       next(error);
     }
-  };
+  }
 
-  public logIn = async (req: Request, res: Response, next: NextFunction) => {
+  async signinOtp(req: Request, res: Response, next: NextFunction) {
     try {
-      const userData: User = req.body;
-      const { cookie, findUser } = await this.auth.login(userData);
-
-      res.setHeader('Set-Cookie', [cookie]);
-      res.status(200).json({ data: findUser, message: 'login' });
+      const result = await authService.signinOtp(req);
+      res.status(result.status).send(result.data);
     } catch (error) {
       next(error);
     }
-  };
+  }
 
-  public logOut = async (req: RequestWithUser, res: Response, next: NextFunction) => {
+  async signup(req: Request, res: Response, next: NextFunction) {
     try {
-      const userData: User = req.user;
-      const logOutUserData: User = await this.auth.logout(userData);
-
-      res.setHeader('Set-Cookie', ['Authorization=; Max-age=0']);
-      res.status(200).json({ data: logOutUserData, message: 'logout' });
+      const result = await authService.signup(req);
+      res.status(result.status).send(result.data);
     } catch (error) {
       next(error);
     }
-  };
+  }
+
+  async social(req: Request, res: Response, next: NextFunction) {
+    try {
+      const result = await authService.social(req, res);
+      res.status(result.status).send(result.data);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async magic(req: Request, res: Response, next: NextFunction) {
+    try {
+      const result = await authService.magic(req);
+      res.status(result.status).send(result.data);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async magicVerify(req: Request, res: Response, next: NextFunction) {
+    try {
+      const result = await authService.magicVerify(req);
+      res.status(result.status).send(result.data);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async get(req: Request, res: Response, next: NextFunction) {
+    try {
+      const result = await authService.get(req);
+      res.status(result.status).send(result.data);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async impersonate(req: Request, res: Response, next: NextFunction) {
+    try {
+      const result = await authService.impersonate(req);
+      res.status(result.status).send(result.data);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async switch(req: Request, res: Response, next: NextFunction) {
+    try {
+      const result = await authService.switch(req);
+      res.status(result.status).send(result.data);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async signout(req: Request, res: Response, next: NextFunction) {
+    try {
+      const result = await authService.signout(req);
+      res.status(result.status).send(result.data);
+    } catch (error) {
+      next(error);
+    }
+  }
 }
+
+
+
+export async function signin(req: Request, res: Response, next: NextFunction) {
+  const data = req.body;
+  let userData: User;
+  let useEmail = false; // determine if flow is email or social
+  console.log('data', data);
+  if (data.email) {
+    useEmail = true;
+    data.provider = 'app';
+    utility.validate(data, ['email', 'password']);
+  } else {
+    // using social, extra fields from jwt
+    utility.validate(data, ['token']);
+    const decode = auth.token.verify(data.token);
+    data.provider = decode.provider;
+    data.provider_id = decode.provider_id;
+    data.email = decode.email;
+  }
+
+  // check user exists
+  userData = useEmail
+    ? await user.get(null, data.email)
+    : await user.get(null, null, null, {
+        provider: data.provider,
+        id: data.provider_id,
+      });
+
+  utility.assert(userData, 'Please enter the correct login details', 'email');
+
+  // verify password
+  if (useEmail) {
+    const verified = await user.password.verify(
+      userData.id,
+      userData.account_id,
+      data.password
+    );
+    utility.assert(
+      verified,
+      'Please enter the correct login details',
+      'password'
+    );
+  }
+
+  // get the account
+  const accountData = await account.get(userData.account_id);
+  utility.assert(
+    accountData?.active,
+    'Your account has been deactivated. Please contact support.'
+  );
+
+  // log the sign in and check if it's suspicious
+  const log = await login.create(userData.id, req);
+  const risk: Risk = await login.verify(userData.id, log);
+
+  // block the signin & send a magic link if risk level is 3 or user account is disabled
+  if (useEmail) {
+    if (risk.level === 3 || userData.disabled) {
+      await user.update(userData.id, userData.account_id, { disabled: true });
+      const token = auth.token({ id: userData.id }, null, 300);
+
+      await mail.send({
+        to: userData.email,
+        template: 'blocked_signin',
+        content: {
+          token: token,
+          domain:
+            utility.validateNativeURL(data.magic_view_url) || `${domain}/magic`,
+        },
+      });
+
+      const msg =
+        risk.level === 3
+          ? 'Your sign in attempt has been blocked due to suspicious activity. '
+          : 'Your account has been disabled due to suspicious activity. ';
+
+      return res.status(403).send({
+        message: msg + 'Please check your email for further instructions.',
+      });
+    }
+
+    // notify the user of suspicious log
--- placio-auth/src/routes/auth.route.ts
+++ placio-auth/src/routes/auth.route.ts
@@ -18,5 +18,24 @@ export class AuthRoute implements Routes {
     this.router.post(`${this.path}signup`, ValidationMiddleware(CreateUserDto, 'body' as any), this.auth.signUp);
     this.router.post(`${this.path}login`, ValidationMiddleware(CreateUserDto, 'body' as any), this.auth.logIn);
     this.router.post(`${this.path}logout`, AuthMiddleware, this.auth.logOut);
+    this.router.post(`${this.path}`, limiter(throttle.signin), use(authController.signin));
+
+    this.router.post(`${this.path}otp`, limiter(throttle.signin), use(authController.signin.otp));
+
+    this.router.get(`${this.path}`, AuthMiddleware, use(authController.get));
+
+    this.router.post(`${this.path}magic`, limiter(throttle.signin), use(authController.magic));
+
+    this.router.post(`${this.path}magic/verify`, limiter(throttle.signin), use(authController.magic.verify));
+
+    this.router.post(`${this.path}password/reset/request`, limiter(throttle.password_reset), use(userController.password.reset.request));
+
+    this.router.post(`${this.path}password/reset`, limiter(throttle.password_reset), use(userController.password.reset));
+
+    this.router.post(`${this.path}switch`, AuthMiddleware, use(authController.switch));
+
+    this.router.post(`${this.path}impersonate`, use(authController.impersonate));
+
+    this.router.delete(`${this.path}`, AuthMiddleware, use(authController.signout));
   }
 }
