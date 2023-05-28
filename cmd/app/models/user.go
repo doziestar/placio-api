f1aa8aea61e652ca74055897840f17427c8235de
Chidozie C. Okafor <39489124+doziestar@users.noreply.github.com>
2023-05-28 12:17:26 +0100

    add settings to model

--- cmd/app/models/user.go
+++ cmd/app/models/user.go
@@ -1,17 +1,23 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+)
 
 type User struct {
 	gorm.Model
 	Auth0ID       string
 	Relationships []UserBusinessRelationship `gorm:"foreignKey:UserID"`
+	Settings GeneralSettings `gorm:"foreignKey:UserID"`
+	Posts []Post `gorm:"foreignKey:UserID"`
 }
 
 type BusinessAccount struct {
 	gorm.Model
 	Name          string
 	Relationships []UserBusinessRelationship `gorm:"foreignKey:BusinessAccountID"`
+	AccountSettings AccountSettings `gorm:"foreignKey:BusinessAccountID"`
+	Posts []Post `gorm:"foreignKey:BusinessAccountID"`
 }
 
 type UserBusinessRelationship struct {
