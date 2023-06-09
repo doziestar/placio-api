d300d6f09fe5ef294b8a7c22b6a50ca82d8755dd
Chidozie C. Okafor <39489124+doziestar@users.noreply.github.com>
2023-06-09 18:28:41 +0100

    fixing cores

--- cmd/app/start/middleware.go
+++ cmd/app/start/middleware.go
@@ -27,11 +27,11 @@ func Middleware(app *gin.Engine) {
 	app.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"PUT", "PATCH", "POST", "GET", "DELETE", "OPTIONS"},
-		AllowHeaders:     []string{"Accept", "Accept-Language", "Content-Language", "Content-Type", "DPR", "origin", "Access-Control-Allow-Headers", "authorization", "X-Requested-With", "X-CSRF-Token", "X-Auth-Token", "X-Forwarded-For", "X-Real-IP", "X-Forwarded-Proto", "X-Forwarded-Host", "X-Forwarded-Port", "X-Forwarded-Server", "X-Forwarded-By", "X-Request-ID", "X-Correlation-ID", "X-Session-ID", "X-Session-Expires"},
-		ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type", "X-Auth-Token", "X-CSRF-Token", "X-Request-ID", "X-Correlation-ID", "X-Session-ID", "X-Session-Expires"},
+		AllowHeaders:     []string{"Accept", "Accept-Language", "Content-Language", "Content-Type", "DPR", "Origin", "Access-Control-Allow-Headers", "Authorization", "X-Requested-With", "X-CSRF-Token", "X-Auth-Token", "X-Forwarded-For", "X-Real-IP", "X-Forwarded-Proto", "X-Forwarded-Host", "X-Forwarded-Port", "X-Forwarded-Server", "X-Forwarded-By", "X-Request-ID", "X-Correlation-ID", "X-Session-ID", "X-Session-Expires"},
+		ExposeHeaders:    []string{"Content-Length", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Content-Type", "X-Auth-Token", "X-CSRF-Token", "X-Request-ID", "X-Correlation-ID", "X-Session-ID", "X-Session-Expires", "Authorization", "X-Forwarded-For", "X-Real-IP", "X-Forwarded-Proto", "X-Forwarded-Host", "X-Forwarded-Port", "X-Forwarded-Server", "X-Forwarded-By"},
 		AllowCredentials: true,
 		AllowOriginFunc: func(origin string) bool {
-			return origin == "https://placio.io"
+			return origin == "*"
 		},
 		MaxAge:     12 * time.Hour,
 		AllowFiles: true,
