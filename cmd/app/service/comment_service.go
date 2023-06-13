006e0856e8afce6a71fd246a7c31276a120e8668
Chidozie C. Okafor <39489124+doziestar@users.noreply.github.com>
2023-06-13 23:44:04 +0100

    working on post and comments

--- cmd/app/service/comment_service.go
+++ cmd/app/service/comment_service.go
@@ -1,66 +1,114 @@
 package service
 
 import (
-	"errors"
-	"placio-app/models"
-
-	"gorm.io/gorm"
+	"context"
+	"fmt"
+	"github.com/google/uuid"
+	"placio-app/ent"
+	"placio-app/ent/comment"
+	"placio-app/ent/post"
+	"placio-app/ent/user"
 )
 
 type CommentService interface {
-	CreateComment(comment *models.Comment) (*models.Comment, error)
-	GetComment(commentID string) (*models.Comment, error)
-	UpdateComment(comment *models.Comment) (*models.Comment, error)
-	DeleteComment(commentID string) error
-	ListComments(postID string) ([]*models.Comment, error)
+	CreateComment(ctx context.Context, userID string, postID string, content string) (*ent.Comment, error)
+	// GetComments GetComment(ctx context.Context, commentID string) (*ent.Comment, error)
+	UpdateComment(ctx context.Context, userID string, commentID string, newContent string) (*ent.Comment, error)
+	DeleteComment(ctx context.Context, userID string, commentID string) error
 }
 
 type CommentServiceImpl struct {
-	db           *gorm.DB
-	store        *models.Comment
-	// accountStore *models.Account
+	client *ent.Client
 }
 
-func NewCommentService(db *gorm.DB) CommentService {
-	return &CommentServiceImpl{db: db, store: &models.Comment{},}
+func NewCommentService(client *ent.Client) CommentService {
+	return &CommentServiceImpl{client: client}
 }
 
-func (cs *CommentServiceImpl) CreateComment(comment *models.Comment) (*models.Comment, error) {
-	if err := cs.db.Create(&comment).Error; err != nil {
-		return nil, err
+func (cs *CommentServiceImpl) CreateComment(ctx context.Context, userID string, postID string, content string) (*ent.Comment, error) {
+	// Check if user exists
+	uExists, err := cs.client.User.Query().Where(user.ID(userID)).Exist(ctx)
+	if err != nil || !uExists {
+		return nil, fmt.Errorf("user does not exist: %w", err)
 	}
+
+	// Check if post exists
+	pExists, err := cs.client.Post.Query().Where(post.ID(postID)).Exist(ctx)
+	if err != nil || !pExists {
+		return nil, fmt.Errorf("post does not exist: %w", err)
+	}
+
+	user, err := cs.client.User.Get(ctx, userID)
+	if err != nil {
+		return nil, fmt.Errorf("failed retrieving user: %w", err)
+	}
+
+	post, err := cs.client.Post.Get(ctx, postID)
+	if err != nil {
+		return nil, fmt.Errorf("failed retrieving post: %w", err)
+	}
+
+	comment, err := cs.client.Comment.
+		Create().
+		SetID(uuid.New().String()).
+		SetContent(content).
+		SetUser(user).
+		SetPost(post).
+		Save(ctx)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed adding comment: %w", err)
+	}
+
 	return comment, nil
 }
 
-func (cs *CommentServiceImpl) GetComment(commentID string) (*models.Comment, error) {
-	var comment models.Comment
-	if err := cs.db.First(&comment, "id = ?", commentID).Error; err != nil {
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			return nil, nil
-		}
-		return nil, err
-	}
-	return &comment, nil
-}
+func (cs *CommentServiceImpl) UpdateComment(ctx context.Context, userID string, commentID string, newContent string) (*ent.Comment, error) {
+	comment, err := cs.client.Comment.
+		Query().
+		Where(comment.ID(commentID)).
+		WithUser().
+		Only(ctx)
 
-func (cs *CommentServiceImpl) UpdateComment(comment *models.Comment) (*models.Comment, error) {
-	if err := cs.db.Model(&models.Comment{}).Where("id = ?", comment.ID).Updates(comment).Error; err != nil {
-		return nil, err
+	if err != nil {
+		return nil, fmt.Errorf("failed retrieving comment: %w", err)
 	}
+
+	if comment.Edges.User.ID != userID {
+		return nil, fmt.Errorf("unauthorized")
+	}
+
+	comment, err = cs.client.Comment.
+		UpdateOneID(commentID).
+		SetContent(newContent).
+		Save(ctx)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed updating comment: %w", err)
+	}
+
 	return comment, nil
 }
 
-func (cs *CommentServiceImpl) DeleteComment(commentID string) error {
-	if err := cs.db.Delete(&models.Comment{}, "id = ?", commentID).Error; err != nil {
-		return err
+func (cs *CommentServiceImpl) DeleteComment(ctx context.Context, userID string, commentID string) error {
+	comment, err := cs.client.Comment.
+		Query().
+		Where(comment.ID(commentID)).
+		WithUser().
+		Only(ctx)
+
+	if err != nil {
+		return fmt.Errorf("failed retrieving comment: %w", err)
 	}
+
+	if comment.Edges.User.ID != userID {
+		return fmt.Errorf("unauthorized")
+	}
+
+	err = cs.client.Comment.DeleteOneID(commentID).Exec(ctx)
+	if err != nil {
+		return fmt.Errorf("failed deleting comment: %w", err)
+	}
+
 	return nil
-}
-
-func (cs *CommentServiceImpl) ListComments(postID string) ([]*models.Comment, error) {
-	var comments []*models.Comment
-	if err := cs.db.Where("post_id = ?", postID).Find(&comments).Error; err != nil {
-		return nil, err
-	}
-	return comments, nil
 }
