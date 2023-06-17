d47b6dd5f6ca853fa6336b959753ca098d2bd4af
Chidozie C. Okafor <39489124+doziestar@users.noreply.github.com>
2023-06-17 18:39:01 +0100

    working on places

---
+++ cmd/app/service/reservation_service.go
@@ -0,0 +1,45 @@
+package service
+
+import (
+	"context"
+	"placio-app/ent"
+)
+
+type ReservationService interface {
+	GetReservation(ctx context.Context, reservationID string) (*ent.Reservation, error)
+	CreateReservation(ctx context.Context, reservationData map[string]interface{}) (*ent.Reservation, error)
+	UpdateReservation(ctx context.Context, reservationID string, reservationData map[string]interface{}) (*ent.Reservation, error)
+	DeleteReservation(ctx context.Context, reservationID string) error
+}
+
+type ReservationServiceImpl struct {
+	client *ent.Client
+}
+
+func NewReservationService(client *ent.Client) *ReservationServiceImpl {
+	return &ReservationServiceImpl{client: client}
+}
+
+func (s *ReservationServiceImpl) GetReservation(ctx context.Context, reservationID string) (*ent.Reservation, error) {
+	return s.client.Reservation.Get(ctx, reservationID)
+}
+
+func (s *ReservationServiceImpl) CreateReservation(ctx context.Context, reservationData map[string]interface{}) (*ent.Reservation, error) {
+	return s.client.Reservation.
+		Create().
+		//SetNillableDateTime(reservationData["date_time"].(time.Time)).
+		Save(ctx)
+}
+
+func (s *ReservationServiceImpl) UpdateReservation(ctx context.Context, reservationID string, reservationData map[string]interface{}) (*ent.Reservation, error) {
+	return s.client.Reservation.
+		UpdateOneID(reservationID).
+		//SetNillableDateTime(reservationData["date_time"].(time.Time)).
+		Save(ctx)
+}
+
+func (s *ReservationServiceImpl) DeleteReservation(ctx context.Context, reservationID string) error {
+	return s.client.Reservation.
+		DeleteOneID(reservationID).
+		Exec(ctx)
+}
