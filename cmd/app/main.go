3f45e8176e93dfd4b34117ef23e44111fb073e25
Chidozie C. Okafor <39489124+doziestar@users.noreply.github.com>
2023-08-02 20:28:00 +0100

    fix localhost origin

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -31,7 +31,7 @@ func main() {
 	app := gin.New()
 
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000", "https://placio.io", "https://www.placio.io", "http://127.0.0.1:3000"},
+		AllowOrigins:     []string{"http://localhost:3000", "https://placio.io", "https://www.placio.io"},
 		AllowMethods:     []string{"*"},
 		AllowHeaders:     []string{"*"},
 		ExposeHeaders:    []string{"Content-Length,Content-Type,Authorization,X-CSRF-Token"},
